9b7cc3bbe94613acdbda9d9d1800d3ad4ed6a680
nmbaibhab <nmbaibhab@gmail.com>
2022-03-11 00:48:07 +0530

    tailwind configured and improvements

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
