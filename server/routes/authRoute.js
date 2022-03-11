6e76b0c62181cc3378608e3d20e3d1f87952afcb
Ash-exp <asutosh2000ad@gmail.com>
2022-03-11 22:13:50 +0530

    grammar verfication added

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/auth");
+const { register, login, grammar } = require("../controllers/auth");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/grammar").get(grammar);
 
 module.exports = router;
